ca15f7ba267914269956240628ac8e56921a874c
Clay Logsdon <ClayLogsdon@users.noreply.github.com>
2023-02-20 20:26:55 -0500

    [Add] update

--- wp-content/themes/idm250-theme/footer.php
+++ wp-content/themes/idm250-theme/footer.php
@@ -1,2 +1,26 @@
+<br><br><br><br><br><br>
+
+
+<footer>
+  <div id="footer">
+    <div class="footer2">
+<?php wp_nav_menu(['theme_location' => 'footer-menu']); ?>
+<div class ="icons">
+
+</div></div>
+
+
+<br>
+<p class="copyright"> Copyright&copy; <?php echo date('Y'); ?> 
+<?php bloginfo('name'); ?> - @2023 Bookish WordPress Theme by Clay Logsdon</p>
+<br></div>
+
+
+
+  </footer>
+<?php // https://developer.wordpress.org/reference/functions/wp_footer/?>
+<?php wp_footer(); ?>
+
+
 </body>
 </html>
--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -4,7 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>title</title>
+    <?php // https://developer.wordpress.org/reference/functions/wp_head/;?>
+  <?php wp_head(); ?>
+
+
+
+
+  
 </head>
-<body>
-    
+
+
+<div class="header-banner">
+        <h1>Bookish</h1>
+    </div>
+    <div class="clear"></div>
+    <nav>
+        <div class="site-title">Bookish</div>
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/contact">Contact</a></li>
+        <ul>
+    </nav>
